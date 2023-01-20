2f2708ab9c360e0671f5e2503a7fda14e7c64b14
Alex Geis <siegxela@gmail.com>
2023-01-20 16:45:43 -0700

    updated error.tsx to be a client component, and display the error message onscreen and in console

--- ghosthop/src/app/error.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./error.module.css";
-
-export default function Error() {
-	return (
-		<main className={styles.main}>
-			<div className={styles.description}>somethings fucked...</div>
-		</main>
-	);
-}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,4 @@
-import styles from "./page.module.css";
+import styles from "./contact.module.css";
 
 export default function Contact() {
 	return (
