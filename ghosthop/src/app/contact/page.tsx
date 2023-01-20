b599cc1e801b116107447d1338af9992c7fe0fba
Alex Geis <siegxela@gmail.com>
2023-01-20 16:35:23 -0700

    add loading page and css, and add /contact route in app directory

---
+++ ghosthop/src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Contact() {
+	return (
+		<main className={styles.main}>
+			<div className={styles.description}>Get in touch</div>
+		</main>
+	);
+}
