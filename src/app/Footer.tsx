563337e8dd3c8de2737ac1a3e389785923f2d903
Alex Geis <siegxela@gmail.com>
2023-01-22 16:00:51 -0700

    footer update - replace img tags with next/Image

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,29 +1,53 @@
-import styles from "./Footer.module.css"
-import githubLogo from '../assets/icons/footer/github-mark.svg'
-import linkedInLogo from '../assets/icons/footer/linkedin-round-icon.svg'
-import spotifyLogo from '../assets/icons/footer/spotify-icon.svg'
+import styles from "./Footer.module.css";
+import Image from "next/image";
+import githubLogo from "../assets/icons/footer/github-mark.svg";
+import linkedInLogo from "../assets/icons/footer/linkedin-round-icon.svg";
+import spotifyLogo from "../assets/icons/footer/spotify-icon.svg";
 
 export function Footer() {
-
-    return (
-        <>
-            <div className={styles.wrapper}>
-                <a href="https://github.com/alexgeis" target="_blank">
-                    <div className={styles.iconWrapper}>
-                        <img src={githubLogo} alt="github" />
-                    </div>
-                </a>
-                <a href="https://www.linkedin.com/in/alexngeis/" target="_blank">
-                    <div className={styles.iconWrapper}>
-                        <img src={linkedInLogo} alt="linkedIn" />
-                    </div>
-                </a>
-                <a href="https://open.spotify.com/artist/41yri20ft220T9dx4A6mWa?si=-e-vKZuXSvqnzf3-7tIjDA" target="_blank">
-                    <div className={styles.iconWrapper}>
-                        <img src={spotifyLogo} alt="spotify" />
-                    </div>
-                </a>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className={styles.wrapper}>
+				<a
+					className={styles.footLink}
+					href="https://github.com/alexgeis"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<div className={styles.iconWrapper}>
+						<Image
+							src={githubLogo}
+							alt="github"
+						/>
+					</div>
+				</a>
+				<a
+					className={styles.footLink}
+					href="https://www.linkedin.com/in/alexngeis/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<div className={styles.iconWrapper}>
+						<Image
+							src={linkedInLogo}
+							alt="linkedIn"
+						/>
+					</div>
+				</a>
+				<a
+					className={styles.footLink}
+					href="https://open.spotify.com/artist/41yri20ft220T9dx4A6mWa?si=-e-vKZuXSvqnzf3-7tIjDA"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<div className={styles.iconWrapper}>
+						<Image
+							src={spotifyLogo}
+							alt="spotify"
+						/>
+					</div>
+				</a>
+			</div>
+		</>
+	);
 }
