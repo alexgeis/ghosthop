1694cec8b3da5525bdc792e4163a248e8419f42f
Alex Geis <siegxela@gmail.com>
2023-01-25 11:11:18 -0700

    add instagram logo and replace linkedin logo/link in footer

--- src/app/(components)/Footer.tsx
+++ src/app/(components)/Footer.tsx
@@ -1,7 +1,8 @@
 import styles from "./Footer.module.css";
 import Image from "next/image";
 import githubLogo from "../../assets/icons/footer/github-mark.svg";
-import linkedInLogo from "../../assets/icons/footer/linkedin-round-icon.svg";
+// import linkedInLogo from "../../assets/icons/footer/linkedin-round-icon.svg";
+import instagramLogo from "../../assets/icons/footer/Instagram_Glyph_Gradient_RGB.svg";
 import spotifyLogo from "../../assets/icons/footer/spotify-icon.svg";
 
 export function Footer() {
@@ -10,7 +11,21 @@ export function Footer() {
 			<div className={styles.wrapper}>
 				<a
 					className={styles.footLink}
-					href="https://github.com/alexgeis"
+					href="https://www.instagram.com/ghosthopofficial/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<div className={styles.iconWrapper}>
+						<Image
+							src={instagramLogo}
+							alt="instagram"
+							className={styles.footerImg}
+						/>
+					</div>
+				</a>
+				<a
+					className={styles.footLink}
+					href="https://github.com/alexgeis/ghosthop"
 					target="_blank"
 					rel="noreferrer"
 				>
@@ -22,20 +37,7 @@ export function Footer() {
 						/>
 					</div>
 				</a>
-				<a
-					className={styles.footLink}
-					href="https://www.linkedin.com/in/alexngeis/"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<div className={styles.iconWrapper}>
-						<Image
-							src={linkedInLogo}
-							alt="linkedIn"
-							className={styles.footerImg}
-						/>
-					</div>
-				</a>
+
 				<a
 					className={styles.footLink}
 					href="https://open.spotify.com/artist/41yri20ft220T9dx4A6mWa?si=-e-vKZuXSvqnzf3-7tIjDA"
