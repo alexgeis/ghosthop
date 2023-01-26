d4450ce660ef63d80e4ca71f7c6c35321ac1e109
Alex Geis <siegxela@gmail.com>
2023-01-26 09:54:39 -0700

    update loading screen text

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import styles from "./loading.module.css";
 export default function Loading() {
 	return (
 		<main className={styles.main}>
-			<div className={styles.description}>shit&apos;s loading, relax...</div>
+			<div className={styles.description}>It&apos;s loading, relax...</div>
 		</main>
 	);
 }
