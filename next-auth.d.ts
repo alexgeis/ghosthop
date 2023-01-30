4ecd45f73d799a6cecfe87173d768b90258328cd
Alex Geis <siegxela@gmail.com>
2023-01-30 09:06:17 -0700

    add /api/auth/[...nextauth].ts boilerplate code (need to resolve typescript errors)

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -2,6 +2,14 @@ import "next-auth/jwt";
 
 // Read more at: https://next-auth.js.org/getting-started/typescript#module-augmentation
 
+/** Example on how to extend the built-in session types */
+declare module "next-auth" {
+	interface Session {
+		/** This is an example. You can find me in types/next-auth.d.ts */
+		foo: string;
+	}
+}
+
 declare module "next-auth/jwt" {
 	interface JWT {
 		/** The user's role. */
