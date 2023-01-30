9e8c341808b4c75d6f7cef30e415dd5a1407453a
Alex Geis <siegxela@gmail.com>
2023-01-30 08:26:04 -0700

    add next-auth.d.ts and process.d.ts typescript files - include both into the tsconfig.json

--- src/server/middleware.ts
+++ src/server/middleware.ts
@@ -8,10 +8,10 @@ export default withAuth({
 			if (req.nextUrl.pathname === "/admin") {
 				return token?.userRole === "admin";
 			}
-			// `/me` only requires the user to be logged in
+			// `/example` (other protected route with no special authorization role) only requires the user to be logged in
 			return !!token;
 		},
 	},
 });
 
-export const config = { matcher: ["/admin", "/me"] };
+export const config = { matcher: ["/admin"] };
