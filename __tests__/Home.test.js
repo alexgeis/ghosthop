e4b70d2826609bdd53f34185d6b09cfd7b33061d
Alex Geis <siegxela@gmail.com>
2023-02-02 17:11:08 -0700

    add example test for homepage header

---
+++ __tests__/Home.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Home from "../src/app/page";
+
+describe("App component", () => {
+	it("renders correct heading", () => {
+		const { getByRole } = render(<Home />);
+		expect(getByRole("heading").textContent).toMatch(/ghosthop/i);
+	});
+});
