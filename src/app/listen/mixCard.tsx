5bf2ed2b1ba156c44ce055a03956f3edb960b009
Alex Geis <siegxela@gmail.com>
2023-02-03 20:10:52 -0700

    add mixCard with props template - need to pass props

--- src/app/listen/mixCard.tsx
+++ src/app/listen/mixCard.tsx
@@ -1,5 +1,47 @@
-import React from "react";
+import styles from "./listen.module.css";
+import Link from "next/link";
 
-export default function mixCard() {
-	return <div>mixCard</div>;
+export default function mixCard({
+	iframeSrc,
+	mixUrl,
+	mixTitle,
+	djUrl,
+	djTitle,
+}) {
+	const mixTitleArr = mixTitle.split(" ");
+	console.log(mixTitleArr);
+
+	return (
+		<div className={styles.mixCard}>
+			<iframe
+				style={{ borderRadius: "12px", border: "0px" }}
+				width="100%"
+				height="166"
+				allow="autoplay"
+				src={iframeSrc}
+			></iframe>
+			<div>
+				{/* <Link
+            href={djUrl}
+            title={djTitle}
+            target="_blank"
+        >
+            {djTitle}
+        </Link>{" "}
+        ·{" "} */}
+				<Link
+					href={mixUrl}
+					// title="[rotations | 10.06.2021] - BecomingMachine2 (live mix)"
+					title={mixTitle}
+					target="_blank"
+				>
+					[SERIES TITLE | DATE]
+					<br></br>
+					MIX TITLE WITH SPACES
+					<br></br>
+					(SONG NOTES IN PARENS)
+				</Link>
+			</div>
+		</div>
+	);
 }
