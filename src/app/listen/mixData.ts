30b793a1bdcb536a3c31cb220d1380324ea52d43
Alex Geis <siegxela@gmail.com>
2023-02-04 18:01:56 -0700

    add city of draining dreams and keychains and rainstorms (2 soundcloud mixes)

--- src/app/listen/mixData.ts
+++ src/app/listen/mixData.ts
@@ -58,4 +58,23 @@ export const mixData: MixData[] = [
 		iframeSrc:
 			"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1390488673&color=%2300ffff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
 	},
+	// CITY OF DRAINING DREAMS
+	{
+		mixTitle: "[rotations | 04.05.2021] - city of draining dreams (live mix)",
+		mixUrl: "https://soundcloud.com/enjaofficial/city-of-draining-dreams",
+		djTitle: "enja",
+		djUrl: "https://soundcloud.com/enjaofficial",
+		iframeSrc:
+			"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1025493358&color=%2300ffff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
+	},
+
+	// KEYCHAINS AND RAINSTORMS
+	{
+		mixTitle: "[rotations | 04.07.2021] - keychains and rainstorms (live mix)",
+		mixUrl: "https://soundcloud.com/enjaofficial/keychains-and-rainstorms",
+		djTitle: "enja",
+		djUrl: "https://soundcloud.com/enjaofficial",
+		iframeSrc:
+			"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1024825831&color=%2300ffff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
+	},
 ];
