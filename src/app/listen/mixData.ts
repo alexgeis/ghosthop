9da0313c730d2ee91ab094c8c0440c11dc20ea2b
Alex Geis <siegxela@gmail.com>
2023-02-18 10:03:45 -0700

    update src/app/listen/mixData.ts with new mix titles (removed series tag [])

--- src/app/listen/mixData.ts
+++ src/app/listen/mixData.ts
@@ -3,7 +3,7 @@ import { MixData } from "types/types";
 export const mixData: MixData[] = [
 	// MILK BAR
 	{
-		mixTitle: "[rotations | 06.20.2022] - Milk Bar (2010 ElectroPop Mix)",
+		mixTitle: "Milk Bar (2010 ElectroPop Mix)",
 		mixUrl: "https://soundcloud.com/enjaofficial/milkbar",
 		djTitle: "enja",
 		djUrl: "https://soundcloud.com/enjaofficial",
@@ -12,8 +12,7 @@ export const mixData: MixData[] = [
 	},
 	// ANCIENT ORIGINS OF PRIMORDIALS
 	{
-		mixTitle:
-			"[rotations | 04.08.2021] - ancient origins of primordials (live mix)",
+		mixTitle: "Ancient Origins of Primordials (Live Mix)",
 		mixUrl:
 			"https://soundcloud.com/enjaofficial/ancient-origins-of-primordials",
 		djTitle: "enja",
@@ -23,7 +22,7 @@ export const mixData: MixData[] = [
 	},
 	// ESCAPING THE CAVE
 	{
-		mixTitle: "[rotations | 08.11.2021] - escaping the cave (artistic mix)",
+		mixTitle: "Escaping The Cave (Artistic Mix)",
 		mixUrl: "https://soundcloud.com/enjaofficial/escaping-the-cave",
 		djTitle: "enja",
 		djUrl: "https://soundcloud.com/enjaofficial",
@@ -32,7 +31,7 @@ export const mixData: MixData[] = [
 	},
 	// BECOMING MACHINE 1
 	{
-		mixTitle: "[rotations | 09.29.2021] - BecomingMachine (extended live mix)",
+		mixTitle: "BecomingMachine (Extended Live Mix)",
 		mixUrl: "https://soundcloud.com/enjaofficial/becomingmachine",
 		djTitle: "enja",
 		djUrl: "https://soundcloud.com/enjaofficial",
@@ -41,7 +40,7 @@ export const mixData: MixData[] = [
 	},
 	// BECOMING MACHINE 2
 	{
-		mixTitle: "[rotations | 10.06.2021] - BecomingMachine2 (live mix)",
+		mixTitle: "BecomingMachine2 (Live Mix)",
 		mixUrl:
 			"https://soundcloud.com/enjaofficial/rotations-10062021-becomingmachine2-live-mix",
 		djTitle: "enja",
@@ -51,7 +50,7 @@ export const mixData: MixData[] = [
 	},
 	// VOODOO ENCHANTRANCE
 	{
-		mixTitle: "[rotations | 11.04.2022] - voodoo enchantrance (live mix)",
+		mixTitle: "Voodoo Enchantrance (Live Mix)",
 		mixUrl: "https://soundcloud.com/enjaofficial/voodoo-enchantrance",
 		djTitle: "enja",
 		djUrl: "https://soundcloud.com/enjaofficial",
@@ -60,7 +59,7 @@ export const mixData: MixData[] = [
 	},
 	// CITY OF DRAINING DREAMS
 	{
-		mixTitle: "[rotations | 04.05.2021] - city of draining dreams (live mix)",
+		mixTitle: "City of Draining Dreams (Live Mix)",
 		mixUrl: "https://soundcloud.com/enjaofficial/city-of-draining-dreams",
 		djTitle: "enja",
 		djUrl: "https://soundcloud.com/enjaofficial",
@@ -70,7 +69,7 @@ export const mixData: MixData[] = [
 
 	// KEYCHAINS AND RAINSTORMS
 	{
-		mixTitle: "[rotations | 04.07.2021] - keychains and rainstorms (live mix)",
+		mixTitle: "Keychains and Rainstorms (Live Mix)",
 		mixUrl: "https://soundcloud.com/enjaofficial/keychains-and-rainstorms",
 		djTitle: "enja",
 		djUrl: "https://soundcloud.com/enjaofficial",
